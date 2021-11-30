dc028b3babd3696c2c4c5b1e56e3aa2172055a34
Sunil <snilavarath@gmail.com>
2021-11-30 12:38:42 -0500

    commit tree d88b7cea0c640571adc3a052adbd5f7f9be8c506

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solution here
+function findMatching(array, name) {
+    return array.filter(item =>  item.toLowerCase() === name.toLowerCase())
+}
+
+
+function fuzzyMatch(array, bit) {
+    return array.filter(item =>  item.startsWith(bit))
+}
+
+
+function matchName(array, name) {
+    return array.filter(item => item.name === name)
+}
